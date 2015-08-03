9c9312c08452f26f1e9936bece4a58bb0a3da82b
mcclure15 <m001010111@gmail.com>
2015-08-03 16:47:17 -0700

    wednesday challenge complete

--- WeekOne_CodingChallenges.playground/Contents.swift
+++ WeekOne_CodingChallenges.playground/Contents.swift
@@ -40,22 +40,22 @@ for (i = 1; i < 101; i++){
 
 //Solution to Wednesday's Coding Challenge: Return the number of times that the string "hi" appears anywhere in the given string.
 
-var sampleText = "Hi, Jonah. Hi, Milton. Are we still on for lunch at Shilla? Hello, Caroline. Say hi to your mother for me. Is she still working at the Hilton? Howdy, Jason. Hi, Mary. Hello, Justin. Yo, Thomas! Say hi to the captain for me."
-
+var sampleText = "Hi, Jonah. Hi, Milton. Are we still on for lunch at Shilla? Hello, Caroline. Say hi to your mother for me. Is she still working at the Hilton? Howdy, Jason. Hi, Mary. Hello, Justin. Yo, Thomas! Say hi to the captain for me. HI-YA!"
 
 func lookForHi(textToSearch: String) -> Int {
+  let textToSearchArray = Array(textToSearch)
   var hiCount : Int = 0
-  for character in textToSearch {
+  var i : Int = 0
+  for character in textToSearchArray {
     if character == "h" || character == "H" {
-//      hiCount++
-//      let char = find(textToSearch, character)
-//      let charIndex: Int = distance(textToSearch.startIndex, char!)
-//      let nextCharIndex : Int = charIndex + 1
-//      let isItAnI = nextCharIndex.description
-//      if isItAnI == "i" || isItAnI == "I" {
-//        hiCount++
-//      }
+      if let charIndex = find(textToSearchArray, character) {
+        let charPosition = distance(textToSearchArray.startIndex, charIndex)
+        if textToSearchArray[charPosition + 1] == "i" || textToSearchArray[charPosition + 1] == "I" {
+          hiCount++
+        }
+      }
     }
+    i++
   }
   return hiCount
 }
